8a37d3d533ea7f71701ddafe3f38d9c67ef7758f
Dheemanth Narasimha <dheemanth.simha01@gmail.com>
2022-01-20 16:44:11 +0530

    Complete exercise 1

---
+++ 01_Day_JavaScript_Refresher/exercise-solns/level1/script.js
@@ -0,0 +1,71 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya',
+]
+
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB',
+]
+
+
+// let arr = []
+
+// let arr2 = [1, 2, 3, 4, 5]
+
+// console.log(arr2.length);
+
+// console.log(arr2[0], arr2[2], arr2[4]);
+
+// let mixedDataTypes = [
+//     5, "char", [1, 2, 3], 5.34, undefined
+// ]
+
+// console.log(mixedDataTypes);
+// console.log((mixedDataTypes.length));
+
+
+// let companies = ["Facebook", "Apple", "google", "Microsoft", "Oracle", "Amazon"]
+
+// console.log(companies);
+
+// console.log(companies.length);
+// // companies[0] = "FACEBOOK"
+// console.log(companies);
+// console.log(companies.toString() + " are good IT companies");
+// console.log(companies.includes("Amazon") ? "Found" : "Nope");
+// console.log(companies.toString)
+// console.log(companies.sort());
+// console.log(companies.reverse());
+// console.log(companies.slice(0, 3));
+// console.log(companies.slice(3, 6));
+// console.log(companies.slice(2));
+// companies.shift()
+// console.log(companies);
+
+// companies.splice(3, 1)
+// console.log(companies);
+
+// companies.pop()
+// console.log(companies);
+
+// companies.splice(0, companies.length)
+// console.log(companies);
+
+
+
+
